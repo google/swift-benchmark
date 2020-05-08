636c841874d3e86a86ab7eceb7983c80fc012bce
Dan Zheng <danielzheng@google.com>
2020-05-08 07:30:44 -0700

    NFC: run swift-format.

--- Tests/BenchmarkTests/XCTTestManifests.swift
+++ Tests/BenchmarkTests/XCTTestManifests.swift
@@ -17,7 +17,7 @@ import XCTest
 #if !canImport(ObjectiveC)
     public func allTests() -> [XCTestCaseEntry] {
         return [
-            testCase(StatsTests.allTests),
+            testCase(StatsTests.allTests)
         ]
     }
 #endif
