a952f1d7deed2368805ac29f09da6a676dde8015
Brennan Saeta <saeta@google.com>
2020-05-14 16:47:15 -0700

    Show standard deviation as percentage of median (#27)
    * Round off the standard deviation.
    
    Previously, it would print out a huge number of values past the decimal
    point. This seems silly to me. This change it to just 2 as a starting point.
    (I think ideally in the long term, we should calculate the appropriate
    number of significant figures to not be mis-leading. But we can start here.)
    
    Before:
    
    ```
    name                                                                   time            std                   iterations
    -----------------------------------------------------------------------------------------------------------------------
    NonBlockingCondition: notify one, no waiters                           49.0 ns         ± 218.03171899854937  1000000
    NonBlockingCondition: notify all, no waiters                           48.0 ns         ± 259.1237923013594   1000000
    NonBlockingCondition: preWait, cancelWait                              62.0 ns         ± 318.1394131555278   1000000
    ```
    
    After:
    
    ```
    name                                                                   time            std           iterations
    ---------------------------------------------------------------------------------------------------------------
    NonBlockingCondition: notify one, no waiters                           55.0 ns         ± 141.28      1000000
    NonBlockingCondition: notify all, no waiters                           48.0 ns         ± 285.49      1000000
    NonBlockingCondition: preWait, cancelWait                              63.0 ns         ± 262.61      1000000
    
    ```
    
    * Switch to using percentage of median.
    
    This new representation gives a much more effective summary of the performance
    and variability of the benchmark. It's also much easier on the eyes too!
    
    Along with this, I switched up the way the stats functions were written (to
    instead by computed properties of `Array<Double>`) as that avoided the name
    clashes with using similarly named local variables.
    
    Note: while the median is the right measurement, and standard deviation is a useful way
    of capturing variation, standard deviation and mean are the statistics that usually
    go together.
    
    * Update tests too.

--- Tests/BenchmarkTests/StatsTests.swift
+++ Tests/BenchmarkTests/StatsTests.swift
@@ -26,62 +26,62 @@ final class StatsTests: XCTestCase {
     let v6: [Double] = [21, 42, 84]
 
     func testSum() {
-        XCTAssertEqual(sum(v0), 0)
-        XCTAssertEqual(sum(v1), 1)
-        XCTAssertEqual(sum(v2), 10)
-        XCTAssertEqual(sum(v4), 42)
-        XCTAssertEqual(sum(v5), 63)
+        XCTAssertEqual(v0.sum, 0)
+        XCTAssertEqual(v1.sum, 1)
+        XCTAssertEqual(v2.sum, 10)
+        XCTAssertEqual(v4.sum, 42)
+        XCTAssertEqual(v5.sum, 63)
     }
 
     func testSumSquared() {
         let squared0: [Double] = []
-        XCTAssertEqual(sumSquared(v0), sum(squared0))
+        XCTAssertEqual(v0.sumSquared, squared0.sum)
         let squared1: [Double] = [0.1 * 0.1, 0.2 * 0.2, 0.3 * 0.3, 0.4 * 0.4]
-        XCTAssertEqual(sumSquared(v1), sum(squared1))
+        XCTAssertEqual(v1.sumSquared, squared1.sum)
         let squared2: [Double] = [1, 4, 9, 16]
-        XCTAssertEqual(sumSquared(v2), sum(squared2))
+        XCTAssertEqual(v2.sumSquared, squared2.sum)
         let squared3: [Double] = [1, 4, 9, 16, 25]
-        XCTAssertEqual(sumSquared(v3), sum(squared3))
+        XCTAssertEqual(v3.sumSquared, squared3.sum)
         let squared4: [Double] = [1764]
-        XCTAssertEqual(sumSquared(v4), sum(squared4))
+        XCTAssertEqual(v4.sumSquared, squared4.sum)
         let squared5: [Double] = [441, 1764]
-        XCTAssertEqual(sumSquared(v5), sum(squared5))
+        XCTAssertEqual(v5.sumSquared, squared5.sum)
     }
 
     func testMean() {
-        XCTAssertEqual(mean(v0), 0)
-        XCTAssertEqual(mean(v1), 0.25)
-        XCTAssertEqual(mean(v2), 2.5)
-        XCTAssertEqual(mean(v3), 3)
-        XCTAssertEqual(mean(v4), 42)
-        XCTAssertEqual(mean(v5), 31.5)
+        XCTAssertEqual(v0.mean, 0)
+        XCTAssertEqual(v1.mean, 0.25)
+        XCTAssertEqual(v2.mean, 2.5)
+        XCTAssertEqual(v3.mean, 3)
+        XCTAssertEqual(v4.mean, 42)
+        XCTAssertEqual(v5.mean, 31.5)
     }
 
     func testMedian() {
-        XCTAssertEqual(median(v0), 0)
-        XCTAssertEqual(median(v1), 0.25)
-        XCTAssertEqual(median(v2), 2.5)
-        XCTAssertEqual(median(v3), 3)
-        XCTAssertEqual(median(v4), 42)
-        XCTAssertEqual(median(v5), 31.5)
-        XCTAssertEqual(median(v6), 42)
+        XCTAssertEqual(v0.median, 0)
+        XCTAssertEqual(v1.median, 0.25)
+        XCTAssertEqual(v2.median, 2.5)
+        XCTAssertEqual(v3.median, 3)
+        XCTAssertEqual(v4.median, 42)
+        XCTAssertEqual(v5.median, 31.5)
+        XCTAssertEqual(v6.median, 42)
     }
 
     func testStd() {
         let std0: Double = 0.0
-        XCTAssertEqual(std(v0), std0)
+        XCTAssertEqual(v0.std, std0)
         let std1: Double = 0.1290994448735806
-        XCTAssertEqual(std(v1), std1)
+        XCTAssertEqual(v1.std, std1)
         let std2: Double = 1.2909944487358056
-        XCTAssertEqual(std(v2), std2)
+        XCTAssertEqual(v2.std, std2)
         let std3: Double = 1.5811388300841898
-        XCTAssertEqual(std(v3), std3)
+        XCTAssertEqual(v3.std, std3)
         let std4: Double = 0
-        XCTAssertEqual(std(v4), std4)
+        XCTAssertEqual(v4.std, std4)
         let std5: Double = 14.849242404917497
-        XCTAssertEqual(std(v5), std5)
+        XCTAssertEqual(v5.std, std5)
         let std6: Double = 32.07802986469088
-        XCTAssertEqual(std(v6), std6)
+        XCTAssertEqual(v6.std, std6)
     }
 
     static var allTests = [
