9924583c27cffee80f97957d8bd3a8105bafe9cd
Koki Nishihara <e175774@ie.u-ryukyu.ac.jp>
2020-05-17 03:53:31 +0900

    Update Sources/Benchmark/BenchmarkReporter.swift
    Co-authored-by: Brennan Saeta <brennan.saeta@gmail.com>

--- Sources/Benchmark/BenchmarkReporter.swift
+++ Sources/Benchmark/BenchmarkReporter.swift
@@ -18,7 +18,7 @@ extension String {
     func leftPadding(toLength: Int, withPad: String) -> String {
         let stringLength = self.count
         if stringLength < toLength {
-            return String(repeating:withPad, count: toLength - stringLength) + self
+            return String(repeating: withPad, count: toLength - stringLength) + self
         } else {
             return String(self.suffix(toLength))
         }
